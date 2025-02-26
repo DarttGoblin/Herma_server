cc31c29af8ef138e0326151cc3da7e9f4daa4e0e
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2025-02-26 21:21:57 +0000

    Update Herma.py

--- Herma.py
+++ Herma.py
@@ -5,14 +5,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 # Configure CORS to allow requests from your GitHub Pages domain
-CORS(app)
-#      , resources={
-#     r"/*": {
-#         "origins": ["https://darttgoblin.github.io"],
-#         "methods": ["POST", "OPTIONS"],
-#         "allow_headers": ["Content-Type"]
-#     }
-# })
+CORS(app, resources={
+    r"/*": {
+        "origins": ["https://darttgoblin.github.io"],
+        "methods": ["POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"]
+    }
+})
 
 # Load the model
 with open('Herma.pkl', 'rb') as pipeline_file:
@@ -21,8 +20,13 @@ with open('Herma.pkl', 'rb') as pipeline_file:
 @app.route('/', methods=['POST', 'OPTIONS'])
 def handle_request():
     if request.method == 'OPTIONS':
-        return '', 204  # Return empty response for preflight requests
-    
+        # Handle preflight request
+        response = jsonify()
+        response.headers.add('Access-Control-Allow-Origin', 'https://darttgoblin.github.io')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        return response, 204
+        
     try:
         data = request.get_json()
         user_responses = data.get('user_responses')
