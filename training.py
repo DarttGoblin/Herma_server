f866d59cc1d3761c4533be6373ad5c9cb3e3adb8
DarttGoblin <yassine.bazgour@gmail.com>
2025-05-18 00:21:05 +0100

    Initial commit

--- training.py
+++ training.py
@@ -4,23 +4,16 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://darttgoblin.github.io"}})
+CORS(app)
 
 with open('Herma.pkl', 'rb') as pipeline_file:
     herma = pickle.load(pipeline_file)
 
 @app.route('/', methods=['POST'])
 def handle_request():
-    if request.method == 'OPTIONS':
-        # Handle preflight request
-        response = jsonify({'success': True})
-        response.headers.add('Access-Control-Allow-Origin', 'https://darttgoblin.github.io')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.headers.add('Access-Control-Allow-Methods', 'POST')
-        return response
-    
     data = request.get_json()  
     user_responses = data.get('user_responses')  
+    print(user_responses)
     
     result = herma.predict([user_responses])[0]
     probabilities = herma.predict_proba([user_responses])[0]
